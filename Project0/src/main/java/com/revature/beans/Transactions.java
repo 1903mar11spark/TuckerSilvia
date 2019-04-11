d163703d12b23dff8d349059b1a4f63bc7cdd418
SilviaG27 <isc.silvia.garcia@gmail.com>
2019-04-11 16:29:43 -0400

    Added transactions and transaction logs

--- Project0/src/main/java/com/revature/beans/Transactions.java
+++ Project0/src/main/java/com/revature/beans/Transactions.java
@@ -4,20 +4,30 @@ public class Transactions {
 
 	private int transactionId;
 	private String tType;
-	private int tAmount;
+	private double tAmount;
+	private String tDate;
 	private Accounts accounts;
+	private int accountss;
 	
 	
 	public Transactions() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	public Transactions(int transactionId, String tType, int tAmount, Accounts accounts) {
+	public Transactions(int transactionId, String tType, double tAmount, String tDate, Accounts accounts) {
 		super();
 		this.transactionId = transactionId;
 		this.tType = tType;
 		this.tAmount = tAmount;
+		this.tDate = tDate;
 		this.accounts = accounts;
+	}
+	public Transactions(int transactionId, String tType, double tAmount, String tDate, int acountss) {
+		this.transactionId = transactionId;
+		this.tType = tType;
+		this.tAmount = tAmount;
+		this.tDate = tDate;
+		this.accountss = accountss;
 	}
 	public int getTransactionId() {
 		return transactionId;
@@ -31,11 +41,17 @@ public class Transactions {
 	public void settType(String tType) {
 		this.tType = tType;
 	}
-	public int gettAmount() {
+	public double gettAmount() {
 		return tAmount;
 	}
-	public void settAmount(int tAmount) {
+	public void settAmount(double tAmount) {
 		this.tAmount = tAmount;
+	}
+	public String gettDate() {
+		return tDate;
+	}
+	public void settDate(String tDate) {
+		this.tDate = tDate;
 	}
 	public Accounts getAccounts() {
 		return accounts;
@@ -43,10 +59,21 @@ public class Transactions {
 	public void setAccounts(Accounts accounts) {
 		this.accounts = accounts;
 	}
+	public int getAccountss() {
+		return accountss;
+	}
+	public void setAccountss(int accountss) {
+		this.accountss = accountss;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		// TODO Auto-generated method stub
+		return super.equals(obj);
+	}
 	@Override
 	public String toString() {
-		return "Transactions [transactionId=" + transactionId + ", tType=" + tType + ", tAmount=" + tAmount
-				+ ", accounts=" + accounts + "]";
+		return "Transactions [Transaction Id= " + transactionId + ", Transaction Type= " + tType + ", Transaction Amount=" + tAmount
+				+", Date:" + tDate + ", Account =" + accountss + "]";
 	}
 	
 	
