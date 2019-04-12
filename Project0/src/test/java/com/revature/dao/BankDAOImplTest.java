d14a5990168f3591bcaa60c98eb642b50da1cfa1
Tucker <tuckerbelton@yahoo.com>
2019-04-11 23:44:11 -0400

    MVP

--- Project0/src/test/java/com/revature/dao/BankDAOImplTest.java
+++ Project0/src/test/java/com/revature/dao/BankDAOImplTest.java
@@ -27,21 +27,19 @@ public class BankDAOImplTest {
 
 
 /*	public List<BankUsers> getUsers();
-public boolean login(String user, String pass);
-public int getUserId(String user);
-public List<Accounts> getAccounts(int userId);
-public int privileges(int userID);
-public void newUser(String user, String pass, String fName, String lName);
-public void createAccount(int userId);
-
-
-public double getBalance(int userId, int account);
-public double getBalanceDeposit(int account, int userId);
-public void updateBalance(int account, double balance);
-public void deleteAccount(int userId);
-public boolean existingAccount(int accountId);
-public void newTransaction(String type, double amt,String date, int accountId);
-public List<Transactions>getTransactions(int account);
-public void updateUser(String user, String pass, String fName, String lName, int userId);
-public void deleteUsers();
+1. public boolean login(String user, String pass);
+2. public int getUserId(String user);
+3. public List<Accounts> getAccounts(int userId);
+4. public int privileges(int userID);
+	public void newUser(String user, String pass, String fName, String lName);
+	public void createAccount(int userId);
+5. public double getBalance(int userId, int account);
+6. public double getBalanceDeposit(int account, int userId);
+	public void updateBalance(int account, double balance);
+	public void deleteAccount(int userId);
+7. public boolean existingAccount(int accountId);
+	public void newTransaction(String type, double amt,String date, int accountId);
+8. public List<Transactions>getTransactions(int account);
+	public void updateUser(String user, String pass, String fName, String lName, int userId);
+	public void deleteUsers();
 */
